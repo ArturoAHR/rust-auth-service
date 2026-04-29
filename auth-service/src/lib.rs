9758c75d89fe63f340dd6e8bb3eea151997357fb
Arturo Hernández <arturoaureliohr@gmail.com>
2026-04-28 20:29:13 -0400

    [Sprint 1][Task 4] feat: add auth service endpoints

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 
-use axum::{serve::Serve, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::post, serve::Serve, Router};
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
@@ -13,7 +13,13 @@ pub struct Application {
 impl Application {
     pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
         let assets_dir = ServeDir::new("assets");
-        let router = Router::new().fallback_service(assets_dir);
+        let router = Router::new()
+            .fallback_service(assets_dir)
+            .route("/signup", post(sign_up))
+            .route("/login", post(login))
+            .route("/verify-2fa", post(verify_2fa))
+            .route("/logout", post(logout))
+            .route("/verify-token", post(verify_token));
 
         let listener = TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
@@ -26,4 +32,24 @@ impl Application {
         println!("Listening on {}", &self.address);
         self.server.await
     }
+}
+
+async fn sign_up() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK.into_response()
 }
