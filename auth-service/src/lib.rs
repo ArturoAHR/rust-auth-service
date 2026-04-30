a3bdd5cf02b55bece50fa87df1e8152a3cfa21f2
Arturo Hernández <arturoaureliohr@gmail.com>
2026-04-30 12:03:24 -0400

    [Sprint 2][Task 1] feat: add hashmap user store to app state

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,12 +1,27 @@
-use std::error::Error;
+use std::{error::Error, sync::Arc};
 
-use axum::{http::StatusCode, response::IntoResponse, routing::post, serve::Serve, Router};
-use tokio::net::TcpListener;
+use axum::{routing::post, serve::Serve, Router};
+use tokio::{net::TcpListener, sync::RwLock};
 use tower_http::services::ServeDir;
+
+use crate::services::hashmap_user_store::HashMapUserStore;
 
 pub mod domain;
 pub mod routes;
 pub mod services;
+
+pub type AppStateUserStore = Arc<RwLock<HashMapUserStore>>;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub user_store: AppStateUserStore,
+}
+
+impl AppState {
+    pub fn new(user_store: AppStateUserStore) -> Self {
+        Self { user_store }
+    }
+}
 
 pub struct Application {
     server: Serve<TcpListener, Router, Router>,
@@ -15,7 +30,7 @@ pub struct Application {
 }
 
 impl Application {
-    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+    pub async fn build(app_state: AppState, address: &str) -> Result<Self, Box<dyn Error>> {
         let assets_dir = ServeDir::new("assets");
         let router = Router::new()
             .fallback_service(assets_dir)
@@ -23,7 +38,8 @@ impl Application {
             .route("/login", post(routes::login))
             .route("/verify-2fa", post(routes::verify_2fa))
             .route("/logout", post(routes::logout))
-            .route("/verify-token", post(routes::verify_token));
+            .route("/verify-token", post(routes::verify_token))
+            .with_state(app_state);
 
         let listener = TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
