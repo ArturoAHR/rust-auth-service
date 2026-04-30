a1d9f20a4330fcf66c336a2ba382449a4d3282e5
Arturo Hernández <arturoaureliohr@gmail.com>
2026-04-30 12:12:15 -0400

    [Sprint 2][Task 1] feat: implement user sign up endpoint

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,5 +1,7 @@
-use axum::{http::StatusCode, response::IntoResponse, Json};
-use serde::Deserialize;
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use serde::{Deserialize, Serialize};
+
+use crate::{domain::User, AppState};
 
 #[derive(Deserialize)]
 pub struct SignUpRequest {
@@ -9,6 +11,24 @@ pub struct SignUpRequest {
     pub requires_2fa: bool,
 }
 
-pub async fn sign_up(Json(request): Json<SignUpRequest>) -> impl IntoResponse {
-    StatusCode::OK.into_response()
+pub async fn sign_up(
+    State(state): State<AppState>,
+    Json(request): Json<SignUpRequest>,
+) -> impl IntoResponse {
+    let user = User::new(request.email, request.password, request.requires_2fa);
+
+    let mut user_store = state.user_store.write().await;
+
+    user_store.add_user(user).unwrap();
+
+    let response = Json(SignUpResponse {
+        message: "User created successfully".to_owned(),
+    });
+
+    (StatusCode::CREATED, response)
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+pub struct SignUpResponse {
+    pub message: String,
 }
