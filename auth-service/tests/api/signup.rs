1d02aeded8c04458a9ebb5f413f627a4dbf0f816
Arturo Hernández <arturoaureliohr@gmail.com>
2026-05-03 11:40:34 -0400

    [Sprint 2][Task 2] test: add integration test cases for invalid credentials and user email conflict

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,4 +1,4 @@
-use auth_service::routes::SignUpResponse;
+use auth_service::{routes::SignUpResponse, ErrorResponse};
 use serde_json::json;
 
 use crate::helpers::{get_random_email, TestApp};
@@ -72,5 +72,83 @@ async fn should_return_201_if_valid_input() {
             .await
             .expect("Could not deserialize response body."),
         expected_response
+    )
+}
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    let test_cases = [
+        json!({
+            "email": "",
+            "password": "password123",
+            "requires2FA": false
+        }),
+        json!({
+            "email": "invalid-email",
+            "password": "password123",
+            "requires2FA": false
+        }),
+        json!({
+            "email": random_email,
+            "password": "1234567",
+            "requires2FA": false
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_sign_up(test_case).await;
+
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "Failed for input {:?}",
+            test_case
+        );
+
+        assert_eq!(
+            response
+                .json::<ErrorResponse>()
+                .await
+                .expect("Could not deserialize response body to Error Response")
+                .error,
+            "Invalid credentials".to_owned()
+        )
+    }
+}
+
+#[tokio::test]
+async fn should_return_409_if_email_already_exists() {
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    let request_body = json!({
+        "email": random_email,
+        "password": "password123",
+        "requires2FA": true
+    });
+
+    let _ = app.post_sign_up(&request_body).await;
+
+    let response = app.post_sign_up(&request_body).await;
+
+    assert_eq!(
+        response.status().as_u16(),
+        409,
+        "Should have returned conflict error code for payload: {:?}",
+        request_body
+    );
+
+    assert_eq!(
+        response
+            .json::<ErrorResponse>()
+            .await
+            .expect("Could not deserialize response body to Error Response")
+            .error,
+        "User already exists".to_owned()
     )
 }
