ac80c109fe7ee970d0ce852aa9d1acfd602a8ed0
Arturo Hernández <arturoaureliohr@gmail.com>
2026-05-13 00:25:11 -0400

    [Sprint 6][Task 5] feat: add `POSTMARK_AUTH_TOKEN` environemnt variable

--- auth-service/src/utils/constants.rs
+++ auth-service/src/utils/constants.rs
@@ -6,6 +6,7 @@ pub mod env {
     pub const DATABASE_URL_ENV_VAR: &str = "DATABASE_URL";
     pub const JWT_SECRET_ENV_VAR: &str = "JWT_SECRET";
     pub const REDIS_HOST_NAME_ENV_VAR: &str = "REDIS_HOST_NAME";
+    pub const POSTMARK_AUTH_TOKEN_ENV_VAR: &str = "POSTMARK_AUTH_TOKEN";
 }
 
 pub const JWT_COOKIE_NAME: &str = "jwt";
@@ -19,43 +20,35 @@ pub mod test {
 }
 
 lazy_static! {
-    pub static ref JWT_SECRET: SecretString = get_jwt_secret();
-    pub static ref DATABASE_URL: SecretString = get_database_url();
-    pub static ref REDIS_HOST_NAME: String = get_redis_host();
+    pub static ref JWT_SECRET: SecretString =
+        get_secret_environment_variable(env::JWT_SECRET_ENV_VAR);
+    pub static ref DATABASE_URL: SecretString =
+        get_secret_environment_variable(env::DATABASE_URL_ENV_VAR);
+    pub static ref REDIS_HOST_NAME: String = get_environment_variable(env::REDIS_HOST_NAME_ENV_VAR);
+    pub static ref POSTMARK_AUTH_TOKEN: SecretString =
+        get_secret_environment_variable(env::POSTMARK_AUTH_TOKEN_ENV_VAR);
 }
 
-fn get_jwt_secret() -> SecretString {
+fn get_environment_variable(variable_name: &str) -> String {
     dotenv().ok();
 
-    let secret = std::env::var(env::JWT_SECRET_ENV_VAR).expect("JWT_SECRET must be set.");
+    let variable = std::env::var(variable_name).expect("{variable_name} must be set.");
+
+    if variable.is_empty() {
+        panic!("{variable_name} must not be empty.");
+    }
+
+    variable
+}
+
+fn get_secret_environment_variable(variable_name: &str) -> SecretString {
+    dotenv().ok();
+
+    let secret = std::env::var(variable_name).expect("{variable_name} must be set.");
 
     if secret.is_empty() {
-        panic!("JWT_SECRET must not be empty.");
+        panic!("{variable_name} must not be empty.");
     }
 
     SecretString::new(secret.into_boxed_str())
-}
-
-fn get_database_url() -> SecretString {
-    dotenv().ok();
-
-    let secret = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
-
-    if secret.is_empty() {
-        panic!("DATABASE_URL must not be empty.");
-    }
-
-    SecretString::new(secret.into_boxed_str())
-}
-
-fn get_redis_host() -> String {
-    dotenv().ok();
-
-    let secret = std::env::var(env::REDIS_HOST_NAME_ENV_VAR).expect("REDIS_HOST_NAME must be set.");
-
-    if secret.is_empty() {
-        panic!("REDIS_HOST_NAME must not be empty.");
-    }
-
-    secret
 }
